361fa9b973c48b1f8c2688fcdd9d0fe9d750a7c4
Arvin Xu <arvinx@foxmail.com>
2018-02-15 14:57:25 +0800

    :art: Format code with prettier&eslint

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,24 +1,20 @@
-export default
-{
-  "entry": "src/index.js",
-  "extraBabelPlugins": [
-    "transform-decorators-legacy",
-    ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": true }]
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [
+    'transform-decorators-legacy',
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
   ],
-  "env": {
-    "development": {
-      "extraBabelPlugins": [
-        "dva-hmr"
-      ]
-    }
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+    },
   },
-  "ignoreMomentLocale": true,
-  "theme": "./src/theme.js",
-  "html": {
-    "template": "./src/index.ejs"
+  ignoreMomentLocale: true,
+  theme: './src/theme.js',
+  html: {
+    template: './src/index.ejs',
   },
-  "publicPath": "/",
-  "disableDynamicImport": true,
-  "hash": true
-
-}
+  publicPath: '/',
+  disableDynamicImport: true,
+  hash: true,
+};
