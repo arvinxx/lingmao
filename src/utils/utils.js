74e3d5e08eed2e492ad0ca1b13aef75f70c86c4d
Arvin Xu <arvinx@foxmail.com>
2018-02-16 14:48:19 +0800

    :memo: Add some comments to some source code

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -133,7 +133,8 @@ function getRenderArr(routes) {
 export function getRoutes(path, routerData) {
   let routes = Object.keys(routerData).filter(routePath =>
     routePath.indexOf(path) === 0 && routePath !== path);
-  // Replace path to '' eg. path='user' /user/name => name
+  // 删除前置路径
+  // eg. path='user' /user/name => /name path='/' /dashboard => dashboard
   routes = routes.map(item => item.replace(path, ''));
   // Get the route to be rendered to remove the deep rendering
   const renderArr = getRenderArr(routes);
