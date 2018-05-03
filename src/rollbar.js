241fdad1816bddcbd44447a51b709b71370add68
Arvin Xu <arvinx@foxmail.com>
2018-05-03 13:01:10 +0800

    :construction_worker: Change gitlab CI Config

--- src/rollbar.js
+++ src/rollbar.js
@@ -1,13 +1,1 @@
-import Rollbar from 'rollbar';
-
-// Track error by rollbar.com
-if (location.host === 'preview.pro.ant.design') {
-  Rollbar.init({
-    accessToken: '033ca6d7c0eb4cc1831cf470c2649971',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    payload: {
-      environment: 'production',
-    },
-  });
-}
+// import Rollbar from 'rollbar';
