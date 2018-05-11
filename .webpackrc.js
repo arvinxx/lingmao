f0c8c861a3e987a6a03b0181dccc95186e523fa5
Arvin Xu <arvinx@foxmail.com>
2018-05-12 03:05:56 +0800

    :wrench: Change config to use path alias

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 export default {
   extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
   env: {
@@ -7,7 +9,15 @@ export default {
   },
   ignoreMomentLocale: true,
   theme: './src/theme.js',
-
+  alias: {
+    'pages': path.resolve(__dirname, './src/pages'),
+    'models': path.resolve(__dirname, './src/models'),
+    'common': path.resolve(__dirname, './src/common'),
+    'components': path.resolve(__dirname, './src/components'),
+    'services': path.resolve(__dirname, './src/services'),
+    'styles': path.resolve(__dirname, './src/styles'),
+    'utils': path.resolve(__dirname, './src/utils'),
+  },
   proxy: process.env.NO_PROXY
     ? {
         '/api': {
