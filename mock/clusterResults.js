1b5f10fac288028157a1d723de06e445a656a557
Arvin Xu <arvinx@foxmail.com>
2018-05-19 03:25:19 +0800

    :clown_face: Add persona mock data

---
+++ mock/clusterResults.js
@@ -0,0 +1,54 @@
+import Mock from 'mockjs';
+// const data = Mock.mock({
+//   'data|1-10':[{
+//
+//   }]
+// })
+export default [
+  [
+    {
+      tagId: '23123',
+      value: 'gdfs',
+      tagText: 'bgfnd',
+      tagGroupText: '',
+      tagGroupId: '',
+    },
+    {
+      tagId: '765',
+      value: '454bggf n',
+      tagText: 'rfewcdsc',
+      tagGroupText: '',
+      tagGroupId: '',
+    },
+    {
+      tagId: '342',
+      value: 'gdaeewve',
+      tagText: 'stdsfdsring',
+      tagGroupText: '',
+      tagGroupId: '',
+    },
+  ],
+  [
+    {
+      tagId: '23123',
+      value: 'yjtuynr',
+      tagText: 'rfdweq',
+      tagGroupText: '',
+      tagGroupId: '',
+    },
+    {
+      tagId: '765',
+      value: '324234',
+      tagText: 'string',
+      tagGroupText: '',
+      tagGroupId: '',
+    },
+    {
+      tagId: '342',
+      value: '324234',
+      tagText: 'stdsfdsring',
+      tagGroupText: '',
+      tagGroupId: '',
+    },
+  ],
+];
