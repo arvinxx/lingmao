060cf76e0871071aac3b34e419ae9519ab3999f4
Arvin Xu <arvinx@foxmail.com>
2018-05-19 03:19:27 +0800

    :fire: Clear unused fire

--- src/components/PersonaEditor/editor-ui/index.ts
+++
@@ -1,6 +0,0 @@
-import Trash from './Trash/index';
-import Toolbar from './Toolbar/index';
-import DisplayModeToggle from './DisplayModeToggle';
-import BottomToolbar from './BottomToolbar/index';
-
-export { Trash, Toolbar, DisplayModeToggle, BottomToolbar };
