64021b64773519def9ad034b8d2764b742b77afa
Arvin Xu <arvinx@foxmail.com>
2018-05-26 19:31:21 +0800

    :lipstick: Finish FA ui

--- src/components/DataPanel/ReductionOpts.tsx
+++ src/components/DataPanel/ReductionOpts.tsx
@@ -1,51 +1,34 @@
 import React, { Component } from 'react';
-import { Dropdown, Button, InputNumber, Collapse, Icon, message, Menu, Checkbox } from 'antd';
-import styles from './ReductionOpts.less';
-import { TDim } from '../../models/data';
-import { getBaseUrl } from '../../utils';
+import { Button, InputNumber, Checkbox, Select } from 'antd';
 import router from 'umi/router';
+import { DispatchProp } from 'react-redux';
+import styles from './ReductionOpts.less';
+import { getBaseUrl } from '../../utils';
 
-function handleMenuClick(e) {
-  message.info('Click on menu item.');
-  console.log('click', e);
-}
-function onChange(value) {
-  console.log('changed', value);
-}
-
-const Panel = Collapse.Panel;
+const { Option } = Select;
 const CheckboxGroup = Checkbox.Group;
-const menu2 = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">最大方差法</Menu.Item>
-    <Menu.Item key="2">方法2</Menu.Item>
-    <Menu.Item key="3">方法3</Menu.Item>
-  </Menu>
-);
 
-//dropbox
-const menu = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">抽取信息量不小于</Menu.Item>
-    <Menu.Item key="2">2nd menu item</Menu.Item>
-    <Menu.Item key="3">3rd item</Menu.Item>
-  </Menu>
-);
-const plainOptions = ['相关系数表', '碎石图', '总方差解释', '成分矩阵'];
+const plainOptions = ['相关系数表', '碎石图', '总方差解释'];
 
-interface IReductionOptsProps {
-  dispatch: Function;
+export interface IReductionOptsProps {
   analysisStage: number;
   pathname: string;
   tabStage: string;
 }
-export default class ReductionOpts extends Component<IReductionOptsProps> {
+export default class ReductionOpts extends Component<IReductionOptsProps & DispatchProp> {
   static defaultProps: IReductionOptsProps = {
-    dispatch: () => {},
     analysisStage: 0,
     pathname: '',
     tabStage: '',
   };
+  state = {
+    value: 100,
+    count: 2,
+    method: '1',
+    rotationMethod: '1',
+    diagrams: [],
+  };
+
   finish = () => {
     if (this.props.analysisStage === 5) {
       this.props.dispatch({ type: 'stage/addAnalysisStageCount' });
@@ -61,46 +44,94 @@ export default class ReductionOpts extends Component<IReductionOptsProps> {
     // 路由跳转
     router.push(`${getBaseUrl(this.props.pathname)}/cluster`);
   };
+
+  changeMethod = (method) => {
+    this.setState({ method });
+  };
+  changeRate = (value) => {
+    this.setState({ value });
+  };
+  changeCount = (count) => {
+    this.setState({ count });
+  };
+
+  changeRotation = (rotationMethod) => {
+    this.setState({ rotationMethod });
+  };
+  changeDiagram = (diagrams) => {
+    this.setState({ diagrams });
+  };
   render() {
+    const { value, method, rotationMethod, count, diagrams } = this.state;
     return (
-      <div>
-        <div>
-          <Dropdown overlay={menu}>
-            <Button style={{ marginLeft: 8 }}>
-              请选择降维抽取参数 <Icon type="down" />
-            </Button>
-          </Dropdown>
-          <InputNumber
-            defaultValue={100}
-            min={0}
-            max={100}
-            formatter={(value) => `${value}%`}
-            parser={(value) => Number(value.replace('%', ''))}
-            onChange={onChange}
-          />
+      <div style={{ marginLeft: 24 }}>
+        <div className={styles.method}>
+          <div style={{ marginRight: 8 }}>提取方法</div>
+
+          <Select
+            //@ts-ignore
+            id="method"
+            style={{ width: 130 }}
+            defaultValue="1"
+            value={method}
+            placeholder="请选择降维方法"
+            onChange={this.changeMethod}
+          >
+            <Option value="1">特征值不小于</Option>
+            <Option value="2">聚类个数为</Option>
+          </Select>
+          <div style={{ marginLeft: 8 }}>
+            {method === '1' ? (
+              <InputNumber
+                style={{ width: 72 }}
+                id="input-rate"
+                min={50}
+                max={300}
+                step={10}
+                value={value}
+                formatter={(value) => `${value}%`}
+                parser={(value) => Number(value.replace('%', ''))}
+                onChange={this.changeRate}
+              />
+            ) : (
+              <InputNumber
+                id="input-count"
+                style={{ width: 72 }}
+                value={count}
+                min={1}
+                max={10}
+                onChange={this.changeCount}
+              />
+            )}
+          </div>
         </div>
-        <div>
-          <Collapse bordered={false} defaultActiveKey={[]}>
-            <Panel className={styles.panel} header="高级选项" key="1">
-              <div>
-                <p>显示图表</p>
-                <CheckboxGroup options={plainOptions} defaultValue={[]} onChange={onChange} />
-              </div>
-              <div>
-                <p>旋转方法</p>
-                <Dropdown.Button onClick={handleMenuClick} overlay={menu2}>
-                  请选择方法
-                </Dropdown.Button>
-              </div>
-              <div />
-              <Button type="primary" ghost>
-                生成图表
-              </Button>
-              <Button type="primary" onClick={this.finish}>
-                确认并跳转
-              </Button>
-            </Panel>
-          </Collapse>
+        <div className={styles.rotation}>
+          <div style={{ marginRight: 8 }}>旋转方法</div>
+          <Select
+            //@ts-ignore
+            id="rotation"
+            style={{ width: 130 }}
+            defaultValue="1"
+            placeholder="请选择方法"
+            value={rotationMethod}
+            onChange={this.changeRotation}
+          >
+            <Option value="1">最大方差法</Option>
+            <Option value="2">最大四次方值法</Option>
+            <Option value="3">不旋转</Option>
+          </Select>
+        </div>
+        <div className={styles.diagram}>
+          <div style={{ marginBottom: 8 }}>显示图表</div>
+          <CheckboxGroup options={plainOptions} value={diagrams} onChange={this.changeDiagram} />
+        </div>
+        <div className={styles.buttons}>
+          <Button type="primary" style={{ marginRight: 16 }}>
+            确认
+          </Button>
+          <Button type="primary" ghost onClick={this.finish}>
+            跳转
+          </Button>
         </div>
       </div>
     );
