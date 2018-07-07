4bb3178291ee17611577a601e250232de1c7f794
arvinx <arvinx@foxmail.com>
2018-07-07 21:16:24 +0800

    :wrench: Change project config

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,18 +1,19 @@
 import { resolve } from 'path';
 
 export default {
-  theme: './src/theme.js',
+  theme: './src/styles/themes/theme.js',
   alias: {
-    components: resolve(__dirname, './src/components'),
-    styles: resolve(__dirname, './src/styles'),
-    utils: resolve(__dirname, './src/utils'),
-    services: resolve(__dirname, './src/services'),
+    '@/components': resolve(__dirname, './src/components'),
+    '@/utils': resolve(__dirname, './src/utils'),
+    '@/services': resolve(__dirname, './src/services'),
+    '@/common': resolve(__dirname, './src/common'),
+    '@/assets': resolve(__dirname, './src/assets'),
+    'styles': resolve(__dirname, './src/styles'),
   },
-  es5ImcompatibleVersions: true,
-  proxy: process.env.NO_PROXY
-    ? {
+  proxy: process.env.MOCK
+    ? {}
+    : {
         '/api': 'http://localhost:7001/',
         '/ml': 'http://localhost:9898',
-      }
-    : {},
+      },
 };
