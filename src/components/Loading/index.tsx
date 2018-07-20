f9411a3f1ab0b7025773e236eab42dc0fa19ea19
arvinx <arvinx@foxmail.com>
2018-07-20 15:48:49 +0800

    :fire: Clean unused file

--- src/components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import Spin from './Spin';
-import './index.less';
-
-export default class index extends Component {
-  render() {
-    return <Spin />;
-  }
-}
