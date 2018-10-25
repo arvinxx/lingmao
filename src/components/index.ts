0688d56e6817f1e93d56579362b37bbd7195571f
Arvin Xu <arvinx@foxmail.com>
2018-10-25 22:37:55 +0800

    :tractor: Change Components structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,7 +7,5 @@ export { default as TagGroup } from './TagGroup';
 export { default as ContextRightMenu } from './ContextRightMenu';
 export { default as DimensionList } from './DimensionList';
 export { default as PersonaEditor } from './PersonaEditor';
-export { default as StarPic } from './StarPic';
-export { default as ClusterDisplay } from './ClusterDisplay';
 export { default as DescriptionList } from './DescriptionList';
 export * from './Charts';
