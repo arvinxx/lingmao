a3e5b09e1f1ff20e3607c29713a323c025b14bea
arvinx <arvinx@foxmail.com>
2018-12-28 22:52:20 +0800

    :pencil: Minor change
    配合修复单测,简单修改了组件的部分代码

--- src/pages/Interview/components/TagSelector/Label.tsx
+++ src/pages/Interview/components/TagSelector/Label.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Input, Icon, Popconfirm } from 'antd';
-import { HotKeys } from 'react-hotkeys';
 import { DispatchProp } from 'react-redux';
 
 import styles from './Label.less';
@@ -28,40 +27,38 @@ export default class Label extends Component<ILabelProps & DispatchProp> {
     });
   };
   shiftTagInput = (e) => {
-    const key = e.target.id;
-    e.preventDefault();
-    this.props.dispatch({
-      type: 'label/showTagInput',
-      payload: key,
-    });
+    if (e.key === 'Tab') {
+      e.preventDefault();
+      const key = e.target.id;
+      this.props.dispatch({
+        type: 'label/showTagInput',
+        payload: key,
+      });
+    }
   };
   render() {
     const { labelKey, value } = this.props;
-    const handlers = {
-      tab: this.shiftTagInput,
-    };
     return (
-      <HotKeys handlers={handlers}>
-        <div key={labelKey} className={styles.container}>
-          <Popconfirm
-            title="确认要删除吗?"
-            onConfirm={() => this.deleteLabel(labelKey)}
-            okText="是"
-            cancelText="否"
-          >
-            <Icon type="close" className={styles.delete} />
-          </Popconfirm>
-          <Input
-            id={labelKey}
-            className={styles['exist-key']}
-            value={value}
-            placeholder={value}
-            onChange={(e) => {
-              this.changeLabel(e, labelKey);
-            }}
-          />
-        </div>
-      </HotKeys>
+      <div key={labelKey} className={styles.container}>
+        <Popconfirm
+          title="确认要删除吗?"
+          onConfirm={() => this.deleteLabel(labelKey)}
+          okText="是"
+          cancelText="否"
+        >
+          <Icon type="close" className={styles.delete} />
+        </Popconfirm>
+        <Input
+          id={labelKey}
+          className={styles['exist-key']}
+          value={value}
+          placeholder={value}
+          onKeyDown={this.shiftTagInput}
+          onChange={(e) => {
+            this.changeLabel(e, labelKey);
+          }}
+        />
+      </div>
     );
   }
 }
